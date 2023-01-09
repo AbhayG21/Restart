539c45a139d167eef15b1341a327a394b5afc0e3
Abhay Gupta <abhay19021@gmail.com>
2023-01-10 00:14:17 +0530

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Abhay Gupta on 05/01/23.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
